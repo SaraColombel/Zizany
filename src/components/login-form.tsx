7d3e9be789ba2f565d3783e31a3c95587c26d03a
ColombelSara <sara.colombel@epitech.eu>
2026-01-27 16:08:34 +0100

    page(login):login form page completed

---
+++ src/components/login-form.tsx
@@ -0,0 +1,152 @@
+// useForm
+"use client";
+import * as React from "react";
+import { useForm } from "react-hook-form";
+// ------
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+
+// useForm : Define the shape of the form data
+type LoginValues = {
+  email: string;
+  password: string;
+};
+// ------
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  // useForm here
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginValues>({
+    defaultValues: { email: "", password: "" },
+  });
+
+  async function onSubmit(values: LoginValues) {
+    // IMPORTANT: cookies httpOnly => credentials: "include"
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(values),
+    });
+
+    // // Optionnel mais recommandé: vérifier que /me marche (cookie bien posé)
+    // const me = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/me`, {
+    //   credentials: "include",
+    // });
+
+    // if (!me.ok) {
+    //   setApiError("Login OK but session not found. (Cookie/CORS issue)");
+    //   return;
+    // }
+
+    // Redirection
+    window.location.href = "/servers";
+  }
+  // -----------------
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+
+          {/* HERE useForm */}
+          <form onSubmit={handleSubmit(onSubmit)}>
+          {/* ---------- */}
+
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+
+                {/* HERE useForm */}
+                <Input
+                  id="email"
+                  type="email"
+                  autoComplete="email"
+                  placeholder="mail@example.com"
+                  // react-hook-form branché ici
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                      message: "Invalid email format",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <FieldDescription className="text-destructive">
+                    {errors.email.message}
+                  </FieldDescription>
+                )}
+                {/* ---------- */}
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                {/* HERE useForm */}
+                <Input
+                  id="password"
+                  type="password"
+                  autoComplete="current-password"
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                />
+                {errors.password && (
+                  <FieldDescription className="text-destructive">
+                    {errors.password.message}
+                  </FieldDescription>
+                )}
+                {/* ---------- */}
+              </Field>
+              <Field>
+                {/* HERE useForm */}
+                <Button type="submit" disabled={isSubmitting}>
+                  {isSubmitting ? "Logging in..." : "Login"}
+                </Button>
+                {/* ---------- */}
+
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="/signup">Sign up</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
