2c3ad66d963f2f7de5d7b942023ec66979286258
mathieuverger <mathieu.vergez@epitech.eu>
2026-02-02 12:34:06 +0100

    refactor(session): export session middleware for ws

--- src/infrastructure/http/express/app.ts
+++ src/infrastructure/http/express/app.ts
@@ -4,6 +4,20 @@ import cookieParser from "cookie-parser";
 import apiRouter from "./routes";
 import session from "express-session";
 
+/**Export utilisé côté Socket.IO */
+export const sessionMiddleware = session({
+  secret: "zizany",
+  resave: false,
+  saveUninitialized: false,
+  rolling: true,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+});
+
 export function createApp() {
   const app = express();
 
