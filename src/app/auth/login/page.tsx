9d8d380cbdbe434214046288bdcf585f9413b15e
BOUBEE Jérémy <jeremy.boubee@neo-gi.com>
2026-02-03 17:13:33 +0100

    fix(auth): logout

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,20 +1,6 @@
 import { LoginForm } from "@/components/login-form";
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
 
 export default async function Page() {
-  const cookieStore = cookies();
-  const cookieHeader = (await cookieStore).toString();
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/me`, {
-    headers: {
-      Cookie: cookieHeader,
-    },
-    cache: "no-store",
-  });
-
-  if (res.ok) {
-    return redirect("/servers");
-  }
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
--- src/app/servers/layout.tsx
+++ src/app/servers/layout.tsx
@@ -1,35 +1,48 @@
-import * as React from "react"
-import { AppSidebar } from "@/components/app-sidebar"
-import { SiteHeader } from "@/components/site-header"
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
-import { ServersProvider } from "@/components/servers-context"
+import * as React from "react";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ServersProvider } from "@/components/servers-context";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
+export default async function ServersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = cookies();
+  const cookieHeader = (await cookieStore).toString();
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/me`, {
+    headers: {
+      Cookie: cookieHeader,
+    },
+    cache: "no-store",
+  });
 
-export default function ServersLayout({
-    children,
-    }: {
-    children: React.ReactNode
-    }) {
-    return (
-        <ServersProvider>
-            <SidebarProvider
-                style={
-                {
-                    "--sidebar-width": "calc(var(--spacing) * 60)",
-                    "--header-height": "calc(var(--spacing) * 12)",
-                } as React.CSSProperties
-                }
-            >
-                <AppSidebar variant="inset" />
-                <SidebarInset>
-                <SiteHeader />
-                <div className="flex flex-1 flex-col">
-                    <div className="@container/main flex flex-1 flex-col gap-2">
-                    <div className="flex flex-col">{children}</div>
-                    </div>
-                </div>
-                </SidebarInset>
-            </SidebarProvider>
-        </ServersProvider>
-    )
+  if (res.status === 401) {
+    return redirect("/auth/login");
+  }
+  return (
+    <ServersProvider>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 60)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col">{children}</div>
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </ServersProvider>
+  );
 }
