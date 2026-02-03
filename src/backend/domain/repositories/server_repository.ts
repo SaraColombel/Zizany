58baca98e46c25924cce48385969e9ca000742bf
mathieuverger <mathieu.vergez@epitech.eu>
2026-02-03 10:46:56 +0100

    domain: add update and delete to server repository

--- src/backend/domain/repositories/server_repository.ts
+++ src/backend/domain/repositories/server_repository.ts
@@ -5,4 +5,6 @@ export abstract class ServerRepository {
   abstract find_by_owner_id(owner_id: number): Promise<Server[] | undefined>;
   abstract get_all(): Promise<Server[]>;
   abstract save(payload: Omit<ServerProperties, "id">): Promise<Server>;
+  abstract update(id: number, payload: Partial<Omit<ServerProperties, "id" | "owner_id">>): Promise<Server>;
+  abstract delete(id: number): Promise<void>;
 }
