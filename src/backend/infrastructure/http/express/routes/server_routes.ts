24ba579cfaa2160d9142edfe6a2dcee0cbe770e9
mathieuverger <mathieu.vergez@epitech.eu>
2026-02-03 14:25:21 +0100

    routes: add servers CRUD and membership join/leave/role update

--- src/backend/infrastructure/http/express/routes/server_routes.ts
+++ src/backend/infrastructure/http/express/routes/server_routes.ts
@@ -10,12 +10,22 @@ const channelController = new ChannelController();
 
 router.get("/", serverController.all);
 router.get("/:id", serverController.index);
+
 router.get("/:id/members", membershipController.all);
+router.put("/:id/members/:userId", membershipController.updateRole);
+
 router.get("/:id/channels", channelController.all);
 router.post("/:id/channels", channelController.create);
 router.put("/:id/channels/:channelId", channelController.update);
 router.delete("/:id/channels/:channelId", channelController.delete);
+
+// servers CRUD
 router.post("/", serverController.save);
+router.put("/:id", serverController.update);
+router.delete("/:id", serverController.delete);
+
+// membership actions
 router.post("/:id/join", membershipController.join);
+router.delete("/:id/leave", membershipController.leave);
 
 export default router;
